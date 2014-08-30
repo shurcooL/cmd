c2c223754bc669ce4d6bd7a463077f76ca7513cb
Dmitri Shuralyov <shurcooL@gmail.com>
2014-08-30 11:14:28 -0700

    cmd/table: Remove unnecessary manual search.

--- table/main.go
+++ table/main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 
 	"github.com/marpaia/chef-golang"
-	"github.com/shurcooL/go/gists/gist7651991"
 )
 
 func chefConnect() *chef.Chef {
@@ -49,55 +48,6 @@ func main() {
 			row := row.(map[string]interface{})
 
 			fmt.Println(row["name"])
-		}
-	case len(args) == 2 && args[0] == "ip":
-		c := chefConnect()
-
-		nodes, err := c.GetNodes()
-		if err != nil {
-			panic(err)
-		}
-
-		/*for n, _ := range nodes {
-			node, ok, err := c.GetNode(n)
-			if err != nil {
-				panic(err)
-			}
-			if ok && node.Info.IPAddress == args[1] {
-				fmt.Println(node.Name)
-			} else if ok {
-				fmt.Println("not matched", node.Name, node.Info.IPAddress)
-			} else {
-				fmt.Println("not found!", n)
-			}
-		}*/
-
-		inChan := make(chan interface{})
-		go func() { // This needs to happen in the background because sending input will be blocked on reading output.
-			for n, _ := range nodes {
-				inChan <- n
-			}
-			close(inChan)
-		}()
-		reduceFunc := func(in interface{}) interface{} {
-			n := in.(string)
-			node, ok, err := c.GetNode(n)
-			if err != nil {
-				panic(err)
-			}
-			if ok && node.Info.IPAddress == args[1] {
-				return node.Name
-			} /* else if ok {
-				fmt.Println("not matched", node.Name, node.Info.IPAddress)
-			} else {
-				fmt.Println("not found!", n)
-			}*/
-			return nil
-		}
-		outChan := gist7651991.GoReduce(inChan, 256, reduceFunc)
-
-		for out := range outChan {
-			fmt.Println(out)
 		}
 	default:
 		flag.PrintDefaults()
