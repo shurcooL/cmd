8e6a4d2513bfba50473a652042b5a7b626bf9eb1
Dmitri Shuralyov <shurcooL@gmail.com>
2014-08-30 11:12:56 -0700

    cmd/table: Add mode to search by IP.

--- table/main.go
+++ table/main.go
@@ -7,10 +7,8 @@ import (
 	"os"
 
 	"github.com/marpaia/chef-golang"
-	"github.com/shurcooL/go-goon"
+	"github.com/shurcooL/go/gists/gist7651991"
 )
-
-var _ = goon.Dump
 
 func chefConnect() *chef.Chef {
 	c, err := chef.Connect()
@@ -34,21 +32,73 @@ func main() {
 			panic(err)
 		}
 
-		//fmt.Println(results.Total)
 		for _, row := range results.Rows {
 			row := row.(map[string]interface{})
 
 			fmt.Println(row["name"])
 		}
-	/*case false:
-	c := chefConnect()
+	case len(args) == 2 && args[0] == "ipaddress":
+		c := chefConnect()
 
-	nodes, err := c.GetNodes()
-	if err != nil {
-		panic(err)
-	}
+		results, err := c.Search("node", "ipaddress:"+args[1])
+		if err != nil {
+			panic(err)
+		}
 
-	goon.DumpExpr(nodes)*/
+		for _, row := range results.Rows {
+			row := row.(map[string]interface{})
+
+			fmt.Println(row["name"])
+		}
+	case len(args) == 2 && args[0] == "ip":
+		c := chefConnect()
+
+		nodes, err := c.GetNodes()
+		if err != nil {
+			panic(err)
+		}
+
+		/*for n, _ := range nodes {
+			node, ok, err := c.GetNode(n)
+			if err != nil {
+				panic(err)
+			}
+			if ok && node.Info.IPAddress == args[1] {
+				fmt.Println(node.Name)
+			} else if ok {
+				fmt.Println("not matched", node.Name, node.Info.IPAddress)
+			} else {
+				fmt.Println("not found!", n)
+			}
+		}*/
+
+		inChan := make(chan interface{})
+		go func() { // This needs to happen in the background because sending input will be blocked on reading output.
+			for n, _ := range nodes {
+				inChan <- n
+			}
+			close(inChan)
+		}()
+		reduceFunc := func(in interface{}) interface{} {
+			n := in.(string)
+			node, ok, err := c.GetNode(n)
+			if err != nil {
+				panic(err)
+			}
+			if ok && node.Info.IPAddress == args[1] {
+				return node.Name
+			} /* else if ok {
+				fmt.Println("not matched", node.Name, node.Info.IPAddress)
+			} else {
+				fmt.Println("not found!", n)
+			}*/
+			return nil
+		}
+		outChan := gist7651991.GoReduce(inChan, 256, reduceFunc)
+
+		for out := range outChan {
+			fmt.Println(out)
+		}
 	default:
 		flag.PrintDefaults()
 		os.Exit(2)
