b987aa87ae08eb9b5e93171542aa2042f8cd2cf8
Dmitri Shuralyov <shurcooL@gmail.com>
2014-09-12 01:10:48 -0700

    Add serve_gopherjs_html, serves .html with Go scripts.

--- dump_httpreq/main.go
+++ dump_httpreq/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/shurcooL/go-goon"
 )
 
-var httpFlag = flag.String("http", ":8080", "Listen for HTTP connections on this address")
+var httpFlag = flag.String("http", ":8080", "Listen for HTTP connections on this address.")
 
 func dumpRequestHandler(w http.ResponseWriter, req *http.Request) {
 	dump, err := httputil.DumpRequest(req, true)
---
+++ serve_gopherjs_html/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/shurcooL/go/gopherjs_http"
+)
+
+var httpFlag = flag.String("http", ":8080", "Listen for HTTP connections on this address.")
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		fmt.Fprintln(os.Stderr, "usage: serve_gopherjs_html file.html")
+		return
+	}
+
+	fmt.Printf("serving at http://%s/index.html\n", *httpFlag)
+
+	http.Handle("/index.html", gopherjs_http.HtmlFile(flag.Args()[0]))
+	err := http.ListenAndServe(*httpFlag, nil)
+	if err != nil {
+		panic(err)
+	}
+}
