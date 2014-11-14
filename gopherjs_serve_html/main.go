230e28fe7fa77e2df0799db5d4dbfc142d6c5f05
Dmitri Shuralyov <shurcooL@gmail.com>
2014-11-13 22:04:16 -0800

    Minor formatting.

--- gopherjs_serve_html/main.go
+++ gopherjs_serve_html/main.go
@@ -30,7 +30,8 @@ func main() {
 	fmt.Printf("serving at http://%s/index.html\n", *httpFlag)
 
 	http.Handle("/index.html", gopherjs_http.HtmlFile(flag.Args()[0]))
-	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.Handle("/", http.FileServer(http.Dir("./")))
+
 	err := http.ListenAndServe(*httpFlag, nil)
 	if err != nil {
 		panic(err)
