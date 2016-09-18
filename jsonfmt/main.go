db245985e046b616282015a7ef9b314eb2a81216
Dmitri Shuralyov <shurcooL@gmail.com>
2016-09-17 20:27:10 -0700

    jsonfmt: Simplify code, add note about unusual code.

--- jsonfmt/main.go
+++ jsonfmt/main.go
@@ -15,9 +15,8 @@ func run() error {
 	if err != nil {
 		return err
 	}
-
 	var out bytes.Buffer
-	err = json.Indent(&out, bytes.TrimSpace(in), "", "\t")
+	err = json.Indent(&out, bytes.TrimSpace(in), "", "\t") // Need to trim space in order to consistently print single newline at the end. This is because of bug in json.Indent that couldn't be resolved in a better way due to Go 1 guarantee, see https://github.com/golang/go/issues/13520#issuecomment-162544193.
 	if err != nil {
 		return err
 	}
@@ -25,13 +24,8 @@ func run() error {
 	if err != nil {
 		return err
 	}
-
 	_, err = io.Copy(os.Stdout, &out)
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
 
 func main() {
