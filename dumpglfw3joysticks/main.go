1de545a67ac77dc7f915b56e37a3ffca6758a191
Dmitri Shuralyov <dmitri@shuralyov.com>
2022-07-10 20:47:14 -0400

    dumpglfw3joysticks: update to GLFW 3.3
    Lock initial goroutine to the main system thread as required by GLFW.
    Also add a -gamepad flag to dump state of gamepads.

--- dumpglfw3joysticks/main.go
+++ dumpglfw3joysticks/main.go
@@ -2,31 +2,70 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"runtime"
 	"time"
 
-	"github.com/go-gl/glfw/v3.2/glfw"
+	"github.com/go-gl/glfw/v3.3/glfw"
 	"github.com/shurcooL/go-goon"
 )
 
+var gamepadFlag = flag.Bool("gamepad", false, "Dump state of attached gamepads.")
+
+func init() { runtime.LockOSThread() }
 func main() {
-	err := glfw.Init()
+	flag.Parse()
+
+	err := run()
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func run() error {
+	glfw.InitHint(glfw.JoystickHatButtons, glfw.False)
+	err := glfw.Init()
+	if err != nil {
+		return err
+	}
 	defer glfw.Terminate()
+
+	if *gamepadFlag {
+		for {
+			var present bool
+			for j := glfw.Joystick1; j <= glfw.JoystickLast; j++ {
+				if !j.IsGamepad() {
+					continue
+				}
+				present = true
+				goon.DumpExpr(j)
+				goon.DumpExpr(j.GetGamepadName())
+				goon.DumpExpr(j.GetGamepadState())
+			}
+			if !present {
+				fmt.Println("no gamepads present")
+			}
+
+			time.Sleep(time.Second)
+		}
+	}
 
 	for {
 		var present bool
-		for joy := glfw.Joystick1; joy <= glfw.JoystickLast; joy++ {
-			if glfw.JoystickPresent(joy) {
-				present = true
-				goon.DumpExpr(joy)
-				goon.DumpExpr(glfw.GetJoystickName(joy))
-				goon.DumpExpr(glfw.GetJoystickButtons(joy))
-				goon.DumpExpr(glfw.GetJoystickAxes(joy))
+		for j := glfw.Joystick1; j <= glfw.JoystickLast; j++ {
+			if !j.Present() {
+				continue
 			}
+			present = true
+			goon.DumpExpr(j)
+			goon.DumpExpr(j.GetName())
+			goon.DumpExpr(j.GetGUID())
+			fmt.Println(len(j.GetButtons()), "buttons:")
+			goon.DumpExpr(j.GetButtons())
+			goon.DumpExpr(j.GetHats())
+			goon.DumpExpr(j.GetAxes())
 		}
 		if !present {
 			fmt.Println("no joysticks present")
